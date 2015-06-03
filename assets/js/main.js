7309b6daa911ef696350277bfab143ede8f221a2
jonas-g <jonas.gabrielson@me.com>
2015-06-03 20:31:13 +0200

    Placeholders

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,10 @@
 //javascript
-
+function validateComment () {
+	var name = document.forms.[kommentarForm][userName].value;
+	var email = document.forms.[kommentarForm][userMail].value;
+	var text = document.forms.[kommentarForm][kommentarText].value;
+	if (name == "" || name == null || email = "" || email == null || text == "" || text == null) {
+		alert("Var vänlig fyll i alla fält.");
+		return false;
+	};
+}
