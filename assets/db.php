944afb8befdaee2f03fc52a013ae6c815c9b77c3
jonas-g <jonas.gabrielson@me.com>
2015-06-03 23:43:08 +0200

    db

---
+++ assets/db.php
@@ -0,0 +1,18 @@
+<?php
+$user = 'root';
+$password = 'root';
+$db = 'lab_db';
+$host = 'localhost';
+$port = '8889';
+
+$link = mysqli_init();
+$success = mysqli_real_connect(
+	$link,
+	$host,
+	$user,
+	$password,
+	$db,
+	$port);
+
+var_dump($success);
+?>
